32ce17da09450358f1599356c6de4bd62e229e27
kyle roux <jstacoder@gmail.com>
2013-04-07 16:10:26 -0700

    added base Die class to minigame.dice.py

---
+++ minigame/minigame/dice.py
@@ -0,0 +1,30 @@
+'''
+    minigame.dice.py
+
+    __author__ = 'kyle roux'
+'''
+import random
+
+class Die(object):
+    def __init__(self, num=None):
+        if num is None:
+            raise ValueError
+        else:
+            self.num = num
+        self.roll()
+    
+    def roll(self, num=None):
+        if num is None:
+            num = self.num
+        self.value = random.randrange(1, num)
+    
+    def __str__(self):
+        return str(self.value)
+
+def test():
+    x = Die(6)
+    x.roll()
+    print x
+    
+if __name__ == '__main__':
+    test()
